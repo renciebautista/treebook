4af977715a7dc53ed3369fb42183463f7d575496
Rencie Bautista <rencie.bautista@yahoo.com>
2014-09-05 23:52:01 +0800

    Install devise, add a user, and instal devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception	
+
+  protected
+
+  def configure_permited_parameters
+  	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :profile_name, :email, :password, :password_confirmation, :remember_me)}
+  end
 end
